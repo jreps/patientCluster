52b264a8fdd5e737d11a8761a349c0a9c9825a36
jreps <jreps@its.jnj.com>
2015-10-17 12:19:46 +0100

    fixed bug

--- R/testScript.R
+++ R/testScript.R
@@ -1,27 +1,27 @@
-pw <- NULL
-user <-  NULL
+.testCode <- function() {
+  library(patientCluster)
+  pw <- NULL
+  user <-  NULL
 
-dbms <- "pdw" #"sql server"
-server <- 'JRDUSAPSCTL01' #'RNDUSRDHIT05' #'JRDUSAPSCTL01'# "RNDUSRDHIT07.jnj.com"
-cdmDatabaseSchema <- 'CDM_CPRD_v5.dbo' #'CDM_CPRD.dbo' #"CDM_CPRD_V5" #"cdm_truven_mdc[dr].dbo"
-workDatabaseSchema <- "scratch.dbo"
-port <- 17001 #NULL #17001
-sqlType <- 'pdw' #'sql server'#'pdw'
+  dbms <- "pdw" #"sql server"
+  server <- 'JRDUSAPSCTL01' #'RNDUSRDHIT05' #'JRDUSAPSCTL01'# "RNDUSRDHIT07.jnj.com"
+  cdmDatabaseSchema <- 'CDM_CPRD_v5.dbo' #'CDM_CPRD.dbo' #"CDM_CPRD_V5" #"cdm_truven_mdc[dr].dbo"
+  workDatabaseSchema <- "scratch.dbo"
+  port <- 17001 #NULL #17001
+  sqlType <- 'pdw' #'sql server'#'pdw'
 
-outputFolder <- 'C:/Users/jreps/Documents/test'
-library(DatabaseConnector)
-dbconnection <- DatabaseConnector::createConnectionDetails(dbms = dbms,
-                                                           server = server,
-                                                           user = user,
-                                                           password = pw,
-                                                           port = port,
-                                                           schema = cdmDatabaseSchema)
-
+  outputFolder <- 'S:/temp/patientClusters'
+  dbconnection <- DatabaseConnector::createConnectionDetails(dbms = dbms,
+                                                             server = server,
+                                                             user = user,
+                                                             password = pw,
+                                                             port = port,
+                                                             schema = cdmDatabaseSchema)
 
 
-library(patientCluster)
-runMain(dbconnection, cdmDatabaseSchema, workDatabaseSchema,
-                    outputFolder,
-                    cohortid=413, agegroup=3, gender=8507,type='history',
-                    method='kmeans', clusterSize=10, centerVal=T,
-                    covariatesToInclude=NULL,covariatesToExclude=NULL)
+  runMain(dbconnection, cdmDatabaseSchema, workDatabaseSchema,
+                      outputFolder,
+                      cohortid=413, agegroup=3, gender=8507,type='history',
+                      method='kmeans', clusterSize=10, centerVal=T,
+                      covariatesToInclude=NULL,covariatesToExclude=NULL)
+}
